b7b5d8e4bcad8c5ec81ca1105cd85a26df398cca
amreo <amreo@live.it>
2019-11-18 10:41:20 +0100

    Various fix

--- sql/dbmounted.sql
+++ sql/dbmounted.sql
@@ -13,8 +13,8 @@
 -- You should have received a copy of the GNU General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-set markup html on spool on
-set lines 200 pages 0 feedback off verify off
+set lines 32767 pages 0 feedback off verify off
+set colsep "|||"
 col owner for a30
 col Nome_Acronimo for a8
 col segment_name for a60
@@ -22,6 +22,7 @@ col segment_name for a60
 select 
 (select value from v$parameter where name='db_name') as Nome_DB,
 (select db_unique_name from v$database) as DB_Unique_name,
+(select instance_number from v$instance) as Instance_number,
 (select status from v$instance) as DB_Status,
 ((SELECT version FROM V$INSTANCE)||(select (case when UPPER(banner) like '%EXTREME%' then ' Extreme Edition' when UPPER(banner) like '%ENTERPRISE%' then ' Enterprise Edition' else ' Standard Edition' end) from v$version where rownum=1)) as Versione,
 (SELECT platform_name  FROM V$database) as platform,
@@ -30,20 +31,20 @@ select
 (select value from v$nls_parameters where parameter='NLS_NCHAR_CHARACTERSET') as NCharset,
 (select value from v$parameter where name='db_block_size') as Blocksize,
 (select value from v$parameter where name='cpu_count') as Cpu_count,
-(select rtrim(to_char(value/1024/1024/1024, 'FM9G999G999D999', 'NLS_NUMERIC_CHARACTERS='',.'''),',') from v$parameter where name='sga_target')  as Sga_Target,
-(select rtrim(to_char(value/1024/1024/1024, 'FM9G999G999D999', 'NLS_NUMERIC_CHARACTERS='',.'''),',') from v$parameter where name='pga_aggregate_target') as Pga_Target,
-(select rtrim(to_char(value/1024/1024/1024, 'FM9G999G999D999', 'NLS_NUMERIC_CHARACTERS='',.'''),',') from v$parameter where name='memory_target') as Pga_Target,
-(select rtrim(to_char(value/1024/1024/1024, 'FM9G999G999D999', 'NLS_NUMERIC_CHARACTERS='',.'''),',') from v$parameter where name='sga_max_size') as sga_max_size,
+(select rtrim(to_char(value/1024/1024/1024, 'FM9G999G999D999', 'NLS_NUMERIC_CHARACTERS=''.,'''),',') from v$parameter where name='sga_target')  as Sga_Target,
+(select rtrim(to_char(value/1024/1024/1024, 'FM9G999G999D999', 'NLS_NUMERIC_CHARACTERS=''.,'''),',') from v$parameter where name='pga_aggregate_target') as Pga_Target,
+(select rtrim(to_char(value/1024/1024/1024, 'FM9G999G999D999', 'NLS_NUMERIC_CHARACTERS=''.,'''),',') from v$parameter where name='memory_target') as Pga_Target,
+(select rtrim(to_char(value/1024/1024/1024, 'FM9G999G999D999', 'NLS_NUMERIC_CHARACTERS=''.,'''),',') from v$parameter where name='sga_max_size') as sga_max_size,
 --(select round(sum(bytes/1024/1024/1024)) from dba_segments),
-'N/A',
+'0',
 --((select round(sum(bytes/1024/1024/1024)) from v$datafile)+(select round(sum(bytes/1024/1024/1024)) from v$tempfile)+(select round(sum(bytes/1024/1024/1024)) from v$log)),
-'N/A',
+'0',
 --((select round(sum(decode(autoextensible,'NO',bytes/1024/1024/1024,'YES',maxbytes/1024/1024/1024))) from v$datafile)+(select round(sum(bytes/1024/1024/1024)) from v$tempfile)+(select round(sum(bytes/1024/1024/1024)) from v$log)),
-'N/A',
+'0',
 --(SELECT replace(replace(output,'Elapsed:',''),chr(32), '') FROM TABLE (DBMS_WORKLOAD_REPOSITORY.awr_report_text (:dbid, :inst_num, :bid, :eid, 0)) where rownum <20 and output like '%Elapsed: %'),
 --(SELECT replace(replace(output,'DB Time:',''),chr(32), '') FROM TABLE (DBMS_WORKLOAD_REPOSITORY.awr_report_text (:dbid, :inst_num, :bid, :eid, 0)) where rownum <20 and output like '%DB Time: %'),
 --:elapsed,:dbtime,(select :result from dual),
-'N/A','N/A','N/A',
+'0','0','0',
 (select case when (select count(*) from v$datafile where name like '+%') > 0 then 'Y' else 'N' end as "ASM" from dual ),
 case when ( select count(*) from V$DATAGUARD_CONFIG) > 1 then 'Y' else 'N' end  as "Dataguard"
 from dual;
